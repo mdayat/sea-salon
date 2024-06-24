4288d12b5a95980e3f31d3ec404120d986a8dc09
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-06-24 15:53:34 +0700

    setup chakra ui

---
+++ src/libs/chakraui.ts
@@ -0,0 +1,31 @@
+import { extendBaseTheme, theme as chakraTheme } from "@chakra-ui/react";
+
+const {
+  Button,
+  Input,
+  FormLabel,
+  FormError,
+  Form,
+  Container,
+  Heading,
+  Avatar,
+  Alert,
+} = chakraTheme.components;
+
+export const theme = extendBaseTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+  components: {
+    Button,
+    Input,
+    FormLabel,
+    FormError,
+    Form,
+    Container,
+    Heading,
+    Avatar,
+    Alert,
+  },
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,37 @@
-import "@/styles/globals.css";
+import { Poppins } from "next/font/google";
+import { ChakraBaseProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
+import { Navbar } from "../components/Navbar";
+import { theme } from "../libs/chakraui";
+import "../styles/globals.css";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --font-poppins: ${poppins.style.fontFamily};
+          }
+        `}
+      </style>
+
+      <ChakraBaseProvider theme={theme}>
+        <Navbar />
+
+        <main className="font-poppins">
+          <Component {...pageProps} />
+        </main>
+      </ChakraBaseProvider>
+    </>
+  );
 }
