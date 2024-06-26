09e787bef28edd9597f3c38e61a2a7891be3dd7f
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-06-26 20:54:29 +0700

    feat: create ContactUs section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { ContactUs } from "../components/ContactUs";
 import { OurServices } from "../components/OurServices";
 
 export default function Home() {
   return (
     <>
       <OurServices />
+      <ContactUs />
     </>
   );
 }
